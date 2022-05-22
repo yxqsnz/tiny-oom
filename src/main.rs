0f2e1dc48b1e135faf7652ae3ac7d772eb378a69
yxqsnz <yxqsnz@gmail.com>
2022-05-22 10:42:38 -0300

    feat: Several improvements
    - now use a library to manage /proc
    - uses simple-logger instead of env_logger
    - simple-logger will not run sysrq when avg10 >= MEM_THRESHOLD in default config
    - added a configuration if you want to run sysrq when avg10 >= MEM_THRESHOLD

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,47 @@
-use anyhow::{anyhow, Result};
-use itertools::Itertools;
-use log::LevelFilter;
-use std::{
-    fs::{self, File},
-    io::{BufRead, BufReader},
-    thread::sleep,
-    time::Duration,
-};
+use procfs::{Meminfo, MemoryPressure, ProcResult};
+use std::time::Instant;
+use std::{fs, thread::sleep, time::Duration};
 const SYSRQ_TRIGGER_FILE: &str = "/proc/sysrq-trigger";
-const PSI_MEMORY_FILE: &str = "/proc/pressure/memory";
 const CHECK_INTERVAL: Duration = Duration::from_secs(1);
 const RECOVERY_INTERVAL: Duration = Duration::from_secs(10);
 const MEM_THRESHOLD: f32 = 15.0;
-fn get_avg10_from_string(s: String) -> Option<f32> {
-    s.split_ascii_whitespace()
-        .skip(1)
-        .map(|x| x.split_once("avg10="))
-        .filter_map(|x| Some(x?.1.to_string()))
-        .find_map(|x| x.parse::<f32>().ok())
+const SYSRQ_WHEN_MEMORY_THRESHOLD: bool = false;
+fn get_memory_used(meminfo: &Meminfo) -> Option<u64> {
+    Some(
+        meminfo.mem_total
+            - meminfo.mem_available?
+            - meminfo.mem_free
+            - meminfo.buffers
+            - meminfo.shmem?,
+    )
 }
-fn main() -> Result<()> {
-    env_logger::Builder::new()
-        .filter_level(LevelFilter::Info)
-        .parse_default_env()
-        .init();
-    log::debug!("sysrq trigger file: {SYSRQ_TRIGGER_FILE}");
-    log::debug!("psi memory file: {PSI_MEMORY_FILE}");
-    log::info!("tiny-oom started!");
+fn main() -> ProcResult<()> {
+    let _ = simple_logger::SimpleLogger::new().init();
+    log::info!("tiny-oom started.");
+    log::info!("build config: MEM_THRESHOLD={MEM_THRESHOLD}, SYSRQ_WHEN_MEMORY_THRESHOLD={SYSRQ_WHEN_MEMORY_THRESHOLD}");
     loop {
-        let psi_reader = BufReader::new(File::open(PSI_MEMORY_FILE)?);
-        let avg10 = psi_reader
-            .lines()
-            .skip(1)
-            .map_ok(get_avg10_from_string)
-            .next()
-            .ok_or(anyhow!("no psi data"))??;
-        if avg10 > Some(MEM_THRESHOLD) {
-            log::info!("triggering sysrq");
-            fs::write(SYSRQ_TRIGGER_FILE, "f")?;
-            log::debug!("recovering");
+        let memory_pressure_percent = MemoryPressure::new()?.full.avg10;
+        #[cfg(debug_assertions)]
+        log::debug!("memory pressure: {memory_pressure_percent}%");
+        if memory_pressure_percent > MEM_THRESHOLD {
+            if SYSRQ_WHEN_MEMORY_THRESHOLD {
+                let now = Instant::now();
+                fs::write(SYSRQ_TRIGGER_FILE, "f")?;
+                log::info!("sysrq triggered in {:?}", now.elapsed());
+            } else {
+                log::warn!("memory pressure is too high, but not triggering sysrq; checking memory usage before");
+                let meminfo = Meminfo::new()?;
+                let used = get_memory_used(&meminfo).unwrap_or(0);
+                let used_percent = (100 * used) / meminfo.mem_total;
+
+                if used_percent > 90 {
+                    log::warn!("memory usage is too high, triggering sysrq");
+                    fs::write(SYSRQ_TRIGGER_FILE, "f")?;
+                }
+                log::info!("memory usage: {used_percent}%");
+            }
             sleep(RECOVERY_INTERVAL);
-        } else if avg10 > Some(MEM_THRESHOLD / 2.0) {
-            log::warn!("memory pressure is high");
-            sleep(CHECK_INTERVAL / 2);
-        } else {
-            log::debug!("memory pressure is low, avg = {:?}", avg10);
-            sleep(CHECK_INTERVAL);
         }
+        sleep(CHECK_INTERVAL);
     }
 }
